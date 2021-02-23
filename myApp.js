d93541ecf1965092022d674436aa66b3c61f804a
HOuadhour <HOuadhour@Yandex.com>
2021-02-23 18:09:44 +0100

    Delete One Document Using model.findByIdAndRemove.

--- myApp.js
+++ myApp.js
@@ -85,7 +85,9 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data) => {
+    err ? console.error(err) : done(null, data);
+  });
 };
 
 const removeManyPeople = (done) => {
