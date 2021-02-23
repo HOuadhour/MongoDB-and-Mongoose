f70e436035eb99810ddcccd02e5f5e5a124bd7d2
HOuadhour <HOuadhour@Yandex.com>
2021-02-23 16:29:15 +0100

    Create a Model.

--- myApp.js
+++ myApp.js
@@ -6,7 +6,15 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
-let Person;
+const { Schema } = mongoose;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: Array,
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
