f65c241ade5133b0f44231eaaa21371d981f64d1
HOuadhour <HOuadhour@Yandex.com>
2021-02-23 17:45:10 +0100

    Perform Classic Updates by Running Find, Edit, then Save.

--- myApp.js
+++ myApp.js
@@ -60,8 +60,16 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) {
+      console.error(err);
+    } else {
+      person.favoriteFoods.push(foodToAdd);
+      person.save((err, updated) => {
+        err ? console.error(err) : done(null, updated);
+      });
+    }
+  });
 };
 
 const findAndUpdate = (personName, done) => {
